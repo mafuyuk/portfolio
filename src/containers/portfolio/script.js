1b1b425127a8c4e3acaf3d77f1bec38a75e2f2b4
mafuyuk <mafuyuk.m@gmail.com>
2018-10-12 01:28:21 +0900

    Card component also made extensibility #1

--- src/containers/portfolio/script.js
+++ src/containers/portfolio/script.js
@@ -4,7 +4,37 @@ export default {
     name: "portfolio",
     data () {
         return {
-            tags: ["Go", "Vue.js"]
+            tags: ["Go", "Vue.js"],
+            cardList: [
+                {
+                    imgPath: "/assets/icon.jpg",
+                    title: "タイトル1",
+                    subTitle: "サブタイトル",
+                    techTags: ["Go", "Vue.js"],
+                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
+                },
+                {
+                    imgPath: "/assets/icon.jpg",
+                    title: "タイトル2",
+                    subTitle: "サブタイトル",
+                    techTags: ["AWS"],
+                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
+                },
+                {
+                    imgPath: "/assets/icon.jpg",
+                    title: "タイトル3",
+                    subTitle: "サブタイトル",
+                    techTags: ["Go", "Cassandra", "Kafka"],
+                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
+                },
+                {
+                    imgPath: "/assets/icon.jpg",
+                    title: "タイトル4",
+                    subTitle: "サブタイトル",
+                    techTags: ["Lambda", "Node.js"],
+                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
+                }
+            ]
         }
     },
     components: {
