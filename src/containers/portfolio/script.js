68c211d186623411bf2d94190b0422741b34f06c
mafuyuk <mafuyuk.m@gmail.com>
2018-10-13 22:29:39 +0900

    Use JSON file

--- src/containers/portfolio/script.js
+++ src/containers/portfolio/script.js
@@ -4,37 +4,7 @@ export default {
     name: "portfolio",
     data () {
         return {
-            tags: ["Go", "Vue.js"],
-            cardList: [
-                {
-                    imgPath: "/assets/icon.jpg",
-                    title: "タイトル1",
-                    subTitle: "サブタイトル",
-                    techTags: ["Go", "Vue.js"],
-                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
-                },
-                {
-                    imgPath: "/assets/icon.jpg",
-                    title: "タイトル2",
-                    subTitle: "サブタイトル",
-                    techTags: ["AWS"],
-                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
-                },
-                {
-                    imgPath: "/assets/icon.jpg",
-                    title: "タイトル3",
-                    subTitle: "サブタイトル",
-                    techTags: ["Go", "Cassandra", "Kafka"],
-                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
-                },
-                {
-                    imgPath: "/assets/icon.jpg",
-                    title: "タイトル4",
-                    subTitle: "サブタイトル",
-                    techTags: ["Lambda", "Node.js"],
-                    content: "せつめいせつめいせつめいせつめいせつめいせつめいせつめい",
-                }
-            ]
+            cardList: require('./portfolio.json'),
         }
     },
     components: {
