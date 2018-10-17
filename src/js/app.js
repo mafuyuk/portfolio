10765cdf42a82291ba7a0eef8d061657f4e4d465
mafuyuk <mafuyuk.m@gmail.com>
2018-10-17 22:53:41 +0900

    Updated use google analytics

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics'
 import VueMaterial from 'vue-material';
 import 'vue-material/dist/vue-material.min.css';
 import 'vue-material/dist/theme/default-dark.css';
@@ -8,12 +9,16 @@ import VueRouter from 'vue-router';
 import Header from '../components/header/Header.vue';
 import routes from './routes';
 
-Vue.use(VueRouter);
-Vue.use(VueMaterial);
-
 const router = new VueRouter({
     mode: 'history',
     routes: routes
+});
+
+Vue.use(VueRouter);
+Vue.use(VueMaterial);
+Vue.use(VueAnalytics, {
+    id: 'UA-91802628-3',
+    router
 });
 
 const app = new Vue({
